30657afbf4908590662dfc8e5129462b9cdd0116
TeamAbella060420 <jabella@rscm.com.sa>
2022-04-04 15:38:52 +0300

    Update html-loop.js

--- js/html-loop.js
+++ js/html-loop.js
@@ -1,1 +1,10 @@
 const main = document.querySelector('main');
+
+let html = '';
+
+for ( let i = 0; i <= 100; i+=5 ) {
+    html += `<div>${i}</div>`;
+    main.innerHTML = html;
+    console.log(html)
+}
+
